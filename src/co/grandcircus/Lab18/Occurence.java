0b6f8e6dba399bcd5cd9408353a70ce4f0d0bf22
Nightowl93 <shontini@umich.edu>
2018-08-08 08:58:37 -0400

    Added Big O estimates

--- src/co/grandcircus/Lab18/Occurence.java
+++ src/co/grandcircus/Lab18/Occurence.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 public class Occurence {
 
+	
 	public static void main(String[] args) {
 		int[] numbers = {1, 2, 3, 2, 2, 4, 5, 5, 7, 8, 4, 4, 1, 0, 10};
 		
@@ -21,6 +22,7 @@ public class Occurence {
 	}
 
 
+	//Estimated O(n)...single Loop
 	public static Map<Integer, Integer> getFrequencyHashMap(int[] numbers) {
 		Map<Integer, Integer> frequency = new HashMap<>();
 		for (int num : numbers) {
@@ -33,6 +35,7 @@ public class Occurence {
 		}
 		
 
+	//Estimated O(n^2) ...Nested Loop
 	public static void getAndPrintFrequencyArray(int[] numbers) {
 		Arrays.sort(numbers);
 		
@@ -57,6 +60,7 @@ public class Occurence {
 	}
 	
 	
+//	Estimated O(n)....Single loop
 	public static void printMap(Map<Integer, Integer> frequency) {
 		for (Map.Entry<Integer, Integer> entry : frequency.entrySet()) {
 			System.out.println("[" + entry.getKey() + "] : " + entry.getValue());
